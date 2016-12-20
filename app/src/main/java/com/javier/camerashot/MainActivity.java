84f13f78352c90c839864830b6510158bd3794bf
Javier <jflores71814@yahoo.com>
2016-12-20 09:02:44 -0500

    first commit

---
+++ app/src/main/java/com/javier/camerashot/MainActivity.java
@@ -0,0 +1,49 @@
+package com.javier.camerashot;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bttn;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bttn = (Button) findViewById(R.id.main_btn);
+
+        imageView = (ImageView) findViewById(R.id.main_imgview);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void imgCapture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        }
+    }
+    private void hasSystemFeature(String featureCamera) {
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+}
